281ff1ea68f7dd915d16a926d7e2c9d7bdd366ab
Sam Seban <s.seban@sed-in.com>
2018-04-01 20:56:19 +0200

    Update functions.sh

--- fr/functions.sh
+++ fr/functions.sh
@@ -31,7 +31,7 @@ local jv_pg_wk_result=$(curl -s "$LIMITED_WIKI_QUERY" | jq -r '.')
 local jv_pg_wk_definition=$(echo "$jv_pg_wk_result" | jq -r '.[2][0]')
 
 #détection changement vers prochaine definiton
-if [[ "$jv_pg_wk_definition" =~ "peut désigner :" || $("$jv_pg_wk_definition") < 20 || "$jv_pg_wk_definition" =~ "peut faire référence à :" || "$jv_pg_wk_definition" =~ "désigne notamment :" ]]
+if [[ "$jv_pg_wk_definition" =~ "peut désigner :" || $jv_pg_wk_definition < 20 || "$jv_pg_wk_definition" =~ "peut faire référence à :" || "$jv_pg_wk_definition" =~ "désigne notamment :" ]]
 then
 jv_pg_wk_definition=$(echo "$jv_pg_wk_result" | jq -r '.[2][1]')
 else
